4dd2390a74aefde1cf9a4732784580dfdb06a44b
Jakub Juhas <juhas.jugi@gmail.com>
2019-11-29 01:19:36 +0100

    java

--- java/src/main/java/com/example/demo/services/PillarService.java
+++ java/src/main/java/com/example/demo/services/PillarService.java
@@ -4,7 +4,6 @@ import com.example.demo.model.Pillar;
 import com.example.demo.repository.PillarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.stream.Collectors;
