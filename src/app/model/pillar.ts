79a9fa6c5d9172335a308f5a0f565514e21fc02a
Jakub Juhas <juhas.jugi@gmail.com>
2019-11-29 01:41:33 +0100

    java

--- src/app/model/pillar.ts
+++ src/app/model/pillar.ts
@@ -1,17 +1,4 @@
 export class Pillar {
-  geometry: {
-    coordinates: Array<number>,
-    type: string
-  };
-  properties: {
-    EntityHandle: string,
-    ExtendedEntity: string,
-    Layer: string,
-    Linetype: string,
-    Name: string,
-    SubClasses: string,
-    Text: string,
-    description: string
-  };
-  type: string;
+    coordinates: Array<number>;
+    EntityHandle: string;
 }
--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 import {Pillar} from '../../model/pillar';
-import {Observable, pipe} from 'rxjs';
+import {Observable, pipe, Subscription} from 'rxjs';
 import {flatMap, map, tap} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 // just an interface for type safety.
 class Marker {
@@ -24,31 +25,45 @@ class Marker {
 export class MapComponent implements OnInit {
 // google maps zoom level
   zoom = 8;
-  private pillarsCollection: AngularFirestoreCollection<Pillar>;
-  pillars: Observable<Pillar[]>;
 
+  pillars: Pillar[] = [];
+  pillarsObservable: Observable<Pillar[]>;
   markers: Marker[] = [];
 
   // initial center position for the map
   lat = 51.673858;
   lng = 7.815982;
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
-    console.log('init');
-    this.pillarsCollection = this.afs.collection('data');
-    this.pillars = this.pillarsCollection.valueChanges();
-    this.pillars.pipe(
+    console.log('test');
+    this.getPillars().pipe(
       flatMap((pillars: Pillar[]) => {
-        return pillars.map((value1 => value1.geometry.coordinates));
-      }),
+            return pillars.map((value1 => value1.coordinates));
+          }),
       tap((pillar) => {
         this.markers.push(new Marker(pillar[0], pillar[1]));
       }),
       tap(() => console.log(this.markers))
     ).subscribe();
+    // console.log('init');
+    // this.pillarsCollection = this.afs.collection('data');
+    // this.pillars = this.pillarsCollection.valueChanges();
+    // this.pillars.pipe(
+    //   flatMap((pillars: Pillar[]) => {
+    //     return pillars.map((value1 => value1.geometry.coordinates));
+    //   }),
+    //   tap((pillar) => {
+    //     this.markers.push(new Marker(pillar[0], pillar[1]));
+    //   }),
+    //   tap(() => console.log(this.markers))
+    // ).subscribe();
 
+  }
+
+  getPillars(): Observable<Pillar[]> {
+    return this.httpClient.get<Pillar[]>('http://147.232.191.144:8087/pillar');
   }
 }
 
